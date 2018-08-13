e1c061bd670674ef1645880a9c38a4042b5050df
Sirish Velkour <svelkour@gmail.com>
2018-08-13 15:31:23 -0400

    Delete files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Jenkins-App-Monitoring
-Jenkins Application Monitoring Jobs and Scripts
-Includes batch, powershell and python scripts
--- checkBankHoliday.bat
+++
@@ -1,5 +0,0 @@
-:: Check Bank Holiday Script.bat - Author - Sirish Velkour
-
-@ECHO OFF
-PowerShell.exe -NoProfile -NonInteractive -ExecutionPolicy unrestricted -Command "& {Invoke-Command -ComputerName SDC-SVN -FilePath "\\sdc-svn\c$\jenkins\psscripts\checkBankHoliday.ps1"}" %*
-EXIT /B %errorlevel%
